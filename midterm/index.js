f502f72edb99c00ab4cbe6627522150dadd423f8
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2021-11-03 23:15:21 -0600

    Add files via upload

--- midterm/index.js
+++ midterm/index.js
@@ -1,9 +1,8 @@
 function openTab(evt, tabName) {
   // Declare all variables
-  var i, tabcontent, tablinks, tabcontent2, tabcontent3,tabcontent4,tabcontent5,tabcontent6,tabcontent7;
-
+  var i, tabcontent, tablinks;
   // Get all elements with class="tabcontent" and hide them
-  tabcontent, tabcontent2 = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
